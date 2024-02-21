5648045dad80ebb202fa4a7d76f2263bf5a6c594
Rivaldo <rivaldosd500@gmail.com>
2024-02-21 15:04:38 -0300

    Update script.js

--- backend/frontend/js/script.js
+++ backend/frontend/js/script.js
@@ -1,4 +1,11 @@
 //login elements 
 
 const login = document.querySelector(".login")
-const loginForm = login.querySelector(".login")
+const loginForm = login.querySelector(".login__form")
+const loginInput = login.querySelector("login__input")
+
+const user = { id: ""}
+
+const handleSubmit = (event) => {event.preventDefault}
+
+loginForm.addEventListener("submit", handleSubmit)
