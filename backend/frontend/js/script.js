6fa540dd40aa00b9e69a20ec3eee224dcce4da9f
Rivaldo <rivaldosd500@gmail.com>
2024-02-21 20:50:13 -0300

    3

--- backend/frontend/js/script.js
+++ backend/frontend/js/script.js
@@ -2,10 +2,45 @@
 
 const login = document.querySelector(".login")
 const loginForm = login.querySelector(".login__form")
-const loginInput = login.querySelector("login__input")
+const loginInput = login.querySelector(".login__input")
+const chat = document.querySelector(".chat")
 
-const user = { id: ""}
 
-const handleSubmit = (event) => {event.preventDefault}
+const colors = [
+    "cadetblue",
+    "darkgoldenrod",
+    "darkkhaki",
+    "cornflowerblue",
+    "hotpink",
+    "gold"
+]
+const user = { id: "", name: "", color: ""}
+
+let websocket
+
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex]
+}
+
+
+
+const handleSubmit = (event) => {  event.preventDefault()
+    user.name = loginInput.value
+
+    user.id = crypto.randomUUID()
+
+    user.color = getRandomColor()
+
+    login.style.display = "none"
+
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws://localhost:8080");
+
+    console.log(user)
+}
+
 
 loginForm.addEventListener("submit", handleSubmit)
